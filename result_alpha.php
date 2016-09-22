7fa9af6149b6664b4c0277ce37eb06c0cc8826f5
Yao Yao <yyao39@Yaos-MacBook-Pro.local>
2016-09-22 15:18:04 -0500

    change index.html text formatting in atom

--- result_alpha.php
+++ result_alpha.php
@@ -2,73 +2,73 @@
 <head>
 
 <style>
-table { 
-	width: 750px; 
-	border-collapse: collapse; 
+table {
+	width: 750px;
+	border-collapse: collapse;
 	margin:50px auto;
 	}
 
 /* Zebra striping */
-tr:nth-of-type(odd) { 
-	background: #eee; 
+tr:nth-of-type(odd) {
+	background: #eee;
 	}
 
-th { 
-	background: #3498db; 
-	color: white; 
-	font-weight: bold; 
+th {
+	background: #3498db;
+	color: white;
+	font-weight: bold;
 	}
 
-td, th { 
-	padding: 10px; 
-	border: 1px solid #ccc; 
-	text-align: left; 
+td, th {
+	padding: 10px;
+	border: 1px solid #ccc;
+	text-align: left;
 	font-size: 18px;
 	}
 
-/* 
+/*
 Max width before this PARTICULAR table gets nasty
 This query will take effect for any screen smaller than 760px
 and also iPads specifically.
 */
-@media 
+@media
 only screen and (max-width: 760px),
 (min-device-width: 768px) and (max-device-width: 1024px)  {
 
-	table { 
-	  	width: 100%; 
+	table {
+	  	width: 100%;
 	}
 
 	/* Force table to not be like tables anymore */
-	table, thead, tbody, th, td, tr { 
-		display: block; 
+	table, thead, tbody, th, td, tr {
+		display: block;
 	}
-	
+
 	/* Hide table headers (but not display: none;, for accessibility) */
-	thead tr { 
+	thead tr {
 		position: absolute;
 		top: -9999px;
 		left: -9999px;
 	}
-	
+
 	tr { border: 1px solid #ccc; }
-	
-	td { 
+
+	td {
 		/* Behave  like a "row" */
 		border: none;
-		border-bottom: 1px solid #eee; 
+		border-bottom: 1px solid #eee;
 		position: relative;
-		padding-left: 50%; 
+		padding-left: 50%;
 	}
 
-	td:before { 
+	td:before {
 		/* Now like a table header */
 		position: absolute;
 		/* Top/left values mimic padding */
 		top: 6px;
 		left: 6px;
-		width: 45%; 
-		padding-right: 10px; 
+		width: 45%;
+		padding-right: 10px;
 		white-space: nowrap;
 		/* Label the data */
 		content: attr(data-column);
@@ -109,7 +109,7 @@ $result = $conn->query("SELECT * FROM Glycine_Betaine WHERE `Solute` LIKE '$solu
 echo $functional_group;
 echo $solute;
      // output data of each row
-if ($result->num_rows > 0) { 
+if ($result->num_rows > 0) {
      echo "<table>
 	     <thead>
   	       <tr>
@@ -170,7 +170,7 @@ $conn1->close();
 // search another database
 $conn2 = new mysqli($servername, $username, $password, $dbname);
 $result2 = $conn2->query("SELECT * FROM Proline WHERE `Solute` LIKE '$solute' AND `Functional_Group` LIKE '$functional_group';");
- 
+
 if ($result2->num_rows > 0) {
     echo "<table>
              <thead>
@@ -193,7 +193,7 @@ if ($result2->num_rows > 0) {
           <td>" . $row2["Method"]."</td>
           <td>" . $row2["Author_and_Publication"]. "</td>
         </tr>";
-    }    
+    }
 }
 
 $conn2->close();
@@ -234,4 +234,3 @@ $conn3->close();
 </table>
 </body>
 </html>
-
